80b3e7d30e28770cd519e242be95080df8f92353
Stanley Huang <stanley.huang@uwaterloo.ca>
2018-03-21 22:12:32 -0400

    Add manifest and logos

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,32 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Hack the North PWA POC",
+        short_name: "HTN",
+        start_url: "/?utm_source=homescreen",
+        background_color: "#f1fdff",
+        theme_color: "#00c4ff",
+        display: "standalone",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/img/icon.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `/img/icon-512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
   ],
 }
